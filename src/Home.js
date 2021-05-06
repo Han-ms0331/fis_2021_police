23b1394dc4885149d765b513f4c0aa158d6736f2
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-05-06 13:55:03 +0900

    add_home.js_0506

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App(props) {
+  return(
+      <div class="main">
+            <div class='main_serch'>
+            
+            </div>
+            <div class="main_info">
+
+            </div>
+      </div>
+
+  )
+}
+
+export default App;
