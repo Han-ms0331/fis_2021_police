2965ba891cbae19cc834cf601ddebf064443c20e
JUNYEONG-K <godoly1211@naver.com>
2021-05-06 13:51:06 +0900

    setting_App.js_0506

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './login';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/login.js
+++ src/login.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import App from './App';
 
-class Login_page extends React.Component{
-    render(){
-        return(
-            <form>
-                <input type='text' placeholder='id' />
-                <input type='submit' value="Log in" />
-            </form>
+function Login(){
 
-        )
-    }
+    return(
+        <div className='inner-container'>
+            <div className='header'>Login</div>
+            <div className='box'>
+                <div className='input-group'>
+                    <label className='username'>Username</label>
+                    <input type='text' name='username' className='login-input' placeholder='Username' onChange={setUser}/>
+                </div>
+                
+                <div className='input-group'>
+                    <label className='password'>Password</label>
+                    <input type='password' name='password' className='login-input' placeholder='Password' />
+                </div>
+
+                <button type='button' className='login-btn' onClick={this.show}>Login</button>
+            </div>
+        </div>
+    );
 }
