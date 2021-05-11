c2d5714d227c7afa131af6be0614576d3344889c
digda-5624 <you@example.com>
2021-05-11 13:52:53 +0900

    '13_52'

--- servers/main.js
+++ servers/main.js
@@ -34,14 +34,11 @@ app.use(
     store: new FileStore(),
   })
 );
-//db.connect();
 
-// app.get("/", (req, res) => {
-//   //로그인 화면
-//   fs.readFile("login", "utf-8", (err, data) => {
-//     res.send(data);
-//   });
-// });
+app.get("/", (req, res) => {
+  //로그인 화면
+    res.send('success');
+});
 
 app.post("/login", (req, res) => {
   if (req.session.is_logined === true && req.session.userid !== null) {
