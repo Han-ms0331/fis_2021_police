e5ed946446a1fbf30f945828a6be8ec89b7e743f
digda-5624 <you@example.com>
2021-05-17 10:04:07 +0900

    'fds'

--- servers/dbfunc.js
+++ servers/dbfunc.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const db = require("./dbid").db;
-
-module.exports = {
-    get_data : async function (sql_string) {
-        return new Promise((resolve, reject) => {
-          db.query(sql_string, (error, data) => {
-            if (error) {
-              throw error;
-            }
-            resolve(data);
-          });
-        });
-    },
-    modify_data : async function (sql_string) {
-      return new Promise((resolve, reject) => {
-        db.query(sql_string, (error, data) => {
-          
-        })
-      })
-    }
-}
-
-=======
-const db = require('./dbid').db;
-// db.connect();
-
 module.exports = {
 	get_data: async function (sql_string) {
 		return new Promise((resolve, reject) => {
@@ -37,4 +10,3 @@ module.exports = {
 		});
 	},
 };
->>>>>>> 3f74298dcd0acc26e46341cc63a08356a9f11417
