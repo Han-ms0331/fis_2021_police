998c861ca739aaca61c2efc0ef2e2e3f69e1426e
wonBR <76773366+wonBR@users.noreply.github.com>
2021-05-18 18:32:19 +0900

    sche

--- servers/main.js
+++ servers/main.js
@@ -219,10 +219,10 @@ app.get("/schedule/:date", (req, res) => {
               console.log(error2);
               //   res.send(false);
             }
-
+            console.log(store_center[0].c_name);
             store_schedule.c_name = store_center[0].c_name;
             store_schedule.c_address = store_center[0].c_address;
-            console.log(store_schedule);
+            //console.log(store_schedule);
             return store_schedule;
           }
         );
