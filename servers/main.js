65576ff01b3013d9cc1770a9f9af330252df9c82
wonBR <76773366+wonBR@users.noreply.github.com>
2021-05-18 18:34:46 +0900

    sche

--- servers/main.js
+++ servers/main.js
@@ -160,8 +160,8 @@ app.get("/home/get_agent/:a_region/:visit_date", async (req, res) => {
     `SELECT * FROM agent WHERE agent_id LIKE '%${a_region}%'`,
     async (error, datas) => {
       try {
-          let result = [];
-          datas.forEach(async (element) => {
+        let result = [];
+        datas.forEach(async (element) => {
           let agent_id = element.agent_id;
           let result2 = await dbfunc.get_agent_status(agent_id, visit_date);
           result.push(result2);
@@ -212,6 +212,7 @@ app.get("/schedule/:date", (req, res) => {
       }
 
       let temp_cid = store_schedule.map((data) => {
+        console.log(data);
         db.query(
           `SELECT c_name, c_address FROM center WHERE center_id = ${data.cid}`,
           function (error2, store_center) {
@@ -219,7 +220,7 @@ app.get("/schedule/:date", (req, res) => {
               console.log(error2);
               //   res.send(false);
             }
-            console.log(store_center[0].c_name);
+
             store_schedule.c_name = store_center[0].c_name;
             store_schedule.c_address = store_center[0].c_address;
             //console.log(store_schedule);
