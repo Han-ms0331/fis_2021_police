1fff874cfc02d752da28071d6434833af2d90f43
digda-5624 <you@example.com>
2021-05-18 19:23:55 +0900

    'fsdfdafdadsf'

--- servers/main.js
+++ servers/main.js
@@ -156,10 +156,10 @@ app.post("/home/call_write/:cid", async (req, res) => {
 app.get("/home/get_agent/:a_region/:visit_date", async (req, res) => {
   let a_region = path.parse(req.params.a_region).base;
   let visit_date = path.parse(req.params.visit_date).base;
+  let result = [];
   await db.query(
     `SELECT * FROM agent WHERE agent_id LIKE '%${a_region}%'`,
     async (error, datas) => {
-      let result = [];
       for (let i = 0; datas[i] != null; i++) {
         let agent_id = datas[i].agent_id;
         let result2 = await dbfunc.get_agent_status(agent_id, visit_date);
