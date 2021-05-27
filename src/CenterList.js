81cb13e9fbe05a59d933785707aa6a786eefad15
wonBR <76773366+wonBR@users.noreply.github.com>
2021-05-27 21:29:59 +0900

    whqt

--- src/CenterList.js
+++ src/CenterList.js
@@ -1,45 +1,45 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import './css/centerlist.css';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import "./css/centerlist.css";
 
 function CenterList(props) {
-	let result;
+  let result;
 
-	const getCenterInfo = async () => {
-		result = await axios.get(
-			`http://192.168.0.117:3000/home/${props.uid}/search/${props.data.center_id}`
-		);
-		console.log(result);
+  const getCenterInfo = async () => {
+    result = await axios.get(
+      `http://192.168.0.117:3000/home/${props.uid}/search/${props.data.center_id}`
+    );
+    console.log(result);
 
-		props.setCenterInfo({
-			centerName: props.data.c_name,
-			centerAddr: props.data.c_address,
-			centerPhoneNumber: props.data.c_ph,
-			centerID: props.data.center_id,
-			callState_list: result.data.calls,
-			applyState_list: result.data.applies,
-		});
-	};
+    props.setCenterInfo({
+      centerName: props.data.c_name,
+      centerAddr: props.data.c_address,
+      centerPhoneNumber: props.data.c_ph,
+      centerID: props.data.center_id,
+      callState_list: result.data.calls,
+      applyState_list: result.data.applies,
+    });
+  };
 
-	const onClick = (e) => {
-		e.preventDefault();
-		props.setCurrentResult(props.data.center_id);
-		getCenterInfo();
-		props.setIsLoading_2(false);
-	};
+  const onClick = (e) => {
+    e.preventDefault();
+    props.setCurrentResult(props.data.center_id);
+    getCenterInfo();
+    props.setIsLoading_2(false);
+  };
 
-	return (
-		<div class='main_search_result_list_item'>
-			<div class='center_info'>{props.data.c_name}</div>
-			<div class='center_info'>{props.data.c_address}</div>
-			<div class='center_info'>{props.data.c_ph}</div>
-			<div class='center_info_num'>{props.data.center_id}</div>
-			<div class='center_info_num'>{props.called}</div>
-			<button class='main_search_result_list_btn' onClick={onClick}>
-				선택
-			</button>
-		</div>
-	);
+  return (
+    <div class="main_search_result_list_item">
+      <div class="center_info">{props.data.c_name}</div>
+      <div class="center_info">{props.data.c_address}</div>
+      <div class="center_info">{props.data.c_ph}</div>
+      <div class="center_info_num">{props.data.center_id}</div>
+      <div class="center_info_num">{props.called}</div>
+      <button class="main_search_result_list_btn" onClick={onClick}>
+        선택
+      </button>
+    </div>
+  );
 }
 
 export default CenterList;
