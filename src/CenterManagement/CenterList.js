937abbb1e2a3e201246aa8d516640c9a7b04a936
digda-5624 <you@example.com>
2021-06-21 17:17:21 +0900

    '0617update'

--- src/CenterManagement/CenterList.js
+++ src/CenterManagement/CenterList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import '../css/centerlist.css';
+const isdanger = require("./isdanger");
 
 function CenterList(props) {
 	const {check} = props;
 	let result;
 	let record = '없음';
+	let danger_flag = isdanger.check_danger(props.data.c_address);
 	if (props.data.call_exists !== 0) {
 		record = props.data.call_exists;
 	}
@@ -38,8 +40,28 @@ function CenterList(props) {
 		props.setIsLoading_2(false);
 	};
 	console.log(record);
+	if(danger_flag === 0)
 	return (
 		<div class={'main_search_result_list_item'}>
+			<div class='center_info'>{props.data.c_name}</div>
+			<div class='center_info_addr'>{props.data.c_address}</div>
+			<div class='center_info'>{props.data.c_ph}</div>
+			<div class='center_info_num'>{props.data.center_id}</div>
+			<div class={record === '참여' ? 
+							'center_info_num-participation' : record === '거부' ? 
+									'center_info_num-reject' : record === '보류' ?
+											'center_info_num-thinking' : 'center_info_num'}>
+			{record}</div>
+			<div class='center_info_btn'>
+				<button class='main_search_result_list_btn' onClick={onClick}>
+					선택
+				</button>
+			</div>
+		</div>
+	);
+	else
+	return (
+		<div class= 'main_search_result_list_item danger'>
 			<div class='center_info'>{props.data.c_name}</div>
 			<div class='center_info_addr'>{props.data.c_address}</div>
 			<div class='center_info'>{props.data.c_ph}</div>
--- src/CenterManagement/callState.js
+++ src/CenterManagement/callState.js
@@ -19,7 +19,6 @@ function CallState(props) {
 			<div class='information'>연락 일자:{props.callState_list.date}</div>
 			<div class='information'>인/아웃바운드:{props.callState_list.in_out}</div>
 
-<<<<<<< HEAD
       <div class="information">
         담당자 이메일 주소:{props.callState_list.m_email}
       </div>
@@ -43,31 +42,6 @@ function CallState(props) {
       />
     </div>
   );
-=======
-			<div class='information'>
-				담당자 이메일 주소:{props.callState_list.m_email}
-			</div>
-			<div class='information'>담당자 전화번호:{props.callState_list.m_ph}</div>
-			<div class='information'>
-				시설 참여 여부:{props.callState_list.participation}
-			</div>
-			<div class='information'>
-				기록자 이름:{localStorage.getItem('userName')}
-			</div>
-			<td class='information_etc'>특이사항:{props.callState_list.etc}</td>
-			<div class='call_modify_btn'>
-				<button onClick={openUpdateCall}>수정</button>
-			</div>
-			<UpdateCallState
-				update={update}
-				data={props.callState_list}
-				centerID={props.centerID}
-				closeCancel={closeUpdateCall}
-				setUpdate={setUpdate}
-			/>
-		</div>
-	);
->>>>>>> 224524602e64f70ca624a75f892747150be2e604
 }
 
 export default CallState;
