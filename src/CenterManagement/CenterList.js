3cebcf66e422730f060bf123324a12449894da00
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-06-28 18:56:01 +0900

    change mailing

--- src/CenterManagement/CenterList.js
+++ src/CenterManagement/CenterList.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../css/centerlist.css';
 import CenterUpdate from './CenterUpdate.js';
-import isdanger from"./isdanger";
+import isdanger from './isdanger';
 
 function CenterList(props) {
-	const {check} = props;
+	const { check } = props;
 	const [test, setTest] = useState(true);
 	let result;
 	let record = '없음';
@@ -15,7 +15,7 @@ function CenterList(props) {
 	}
 	console.log(check);
 	console.log(props.check);
-//	console.log(props.data);
+	//	console.log(props.data);
 	const getCenterInfo = async () => {
 		result = await axios.get(
 			`http://192.168.0.117:3000/home/${props.uid}/search/${props.data.center_id}`
@@ -31,64 +31,76 @@ function CenterList(props) {
 		});
 	};
 	const update = (e) => {
-		if(check == true){
+		if (check == true) {
 			onClick();
 		}
-	}
+	};
 	const onClick = (e) => {
-		const data = JSON.stringify(props.data)
-		localStorage.setItem("data", data)
+		const data = JSON.stringify(props.data);
+		localStorage.setItem('data', data);
 		console.log(props.data);
-		props.setCenterInfo(props.data);
+		// props.setCenterInfo(props.data);
 		console.log(check);
 		e.preventDefault();
 		props.setCurrentResult(props.data.center_id);
 		getCenterInfo();
 		props.setIsLoading_2(false);
 		setTest(true);
-		const states = JSON.parse(localStorage.getItem("data"));
+		const states = JSON.parse(localStorage.getItem('data'));
 	};
 	console.log(record);
-	if(danger_flag === 0)
-	return (
-		<div class={'main_search_result_list_item'}>
-			<div class='center_info'>{props.data.c_name}</div>
-			<div class='center_info_addr'>{props.data.c_address}</div>
-			<div class='center_info'>{props.data.c_ph}</div>
-			<div class='center_info_num'>{props.data.center_id}</div>
-			<div class={record === '참여' ? 
-							'center_info_num-participation' : record === '거부' ? 
-									'center_info_num-reject' : record === '보류' ?
-											'center_info_num-thinking' : 'center_info_num'}>
-			{record}</div>
-			<div class='center_info_btn'>
-				<button class='main_search_result_list_btn' onClick={onClick}>
-					선택
-				</button>
+	if (danger_flag === 0)
+		return (
+			<div class={'main_search_result_list_item'}>
+				<div class='center_info'>{props.data.c_name}</div>
+				<div class='center_info_addr'>{props.data.c_address}</div>
+				<div class='center_info'>{props.data.c_ph}</div>
+				<div class='center_info_num'>{props.data.center_id}</div>
+				<div
+					class={
+						record === '참여'
+							? 'center_info_num-participation'
+							: record === '거부'
+							? 'center_info_num-reject'
+							: record === '보류'
+							? 'center_info_num-thinking'
+							: 'center_info_num'
+					}>
+					{record}
+				</div>
+				<div class='center_info_btn'>
+					<button class='main_search_result_list_btn' onClick={onClick}>
+						선택
+					</button>
+				</div>
 			</div>
-
-		</div>
-	);
+		);
 	else
-	return (
-		<div class= 'main_search_result_list_item danger'>
-			<div class='center_info'>{props.data.c_name}</div>
-			<div class='center_info_addr'>{props.data.c_address}</div>
-			<div class='center_info'>{props.data.c_ph}</div>
-			<div class='center_info_num'>{props.data.center_id}</div>
-			<div class={record === '참여' ? 
-							'center_info_num-participation' : record === '거부' ? 
-									'center_info_num-reject' : record === '보류' ?
-											'center_info_num-thinking' : 'center_info_num'}>
-			{record}</div>
-			<div class='center_info_btn'>
-				<button class='main_search_result_list_btn' onClick={onClick}>
-					선택
-				</button>
+		return (
+			<div class='main_search_result_list_item danger'>
+				<div class='center_info'>{props.data.c_name}</div>
+				<div class='center_info_addr'>{props.data.c_address}</div>
+				<div class='center_info'>{props.data.c_ph}</div>
+				<div class='center_info_num'>{props.data.center_id}</div>
+				<div
+					class={
+						record === '참여'
+							? 'center_info_num-participation'
+							: record === '거부'
+							? 'center_info_num-reject'
+							: record === '보류'
+							? 'center_info_num-thinking'
+							: 'center_info_num'
+					}>
+					{record}
+				</div>
+				<div class='center_info_btn'>
+					<button class='main_search_result_list_btn' onClick={onClick}>
+						선택
+					</button>
+				</div>
 			</div>
-
-		</div>
-	);
+		);
 }
 
 export default CenterList;
