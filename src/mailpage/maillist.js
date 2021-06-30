85dec9a1e8b452a274ff9dd08883aaab7b5259f3
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-06-30 17:29:49 +0900

    change css maillist

--- src/mailpage/maillist.js
+++ src/mailpage/maillist.js
@@ -2,10 +2,11 @@ import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router';
 import { element } from 'prop-types';
+import '../css/Maillist.css';
 function Maillist(props) {
 	//  let result_array;
 	// const [result_array, setResult_array] = useState([]);
-    // const result_array = props.result_array;
+	// const result_array = props.result_array;
 	//props.setIsLogined(localStorage.getItem('isLogined'));
 	// const mailinfo = async () => {
 	// 	const result = await axios.get(
@@ -18,18 +19,18 @@ function Maillist(props) {
 	// mailinfo();
 	// const result = await axios.get(`http://192.168.0.117:3000/readingmail/read`);
 	// setResult_array(result);
-    //console.log('xxx');
+	//console.log('xxx');
 	return props.isLogined ? (
-		<div>
+		<div class='maillist_container'>
 			{props.result_array3 &&
 				props.result_array3.map((data) => {
-					return <div>{data}</div>;
+					return <div class='maillist_data'>{data}</div>;
 				})}
 		</div>
 	) : (
 		<Redirect to='/' />
 	);
-    return <div>xxx</div>
+	return <div>xxx</div>;
 }
 
 export default Maillist;
